0cbb4d13e3e80cbfb38c043c8e54ea0289e8ddf8
twa127 <46624596+twa127@users.noreply.github.com>
2022-05-17 14:52:45 +0100

    Add files via upload

--- theme.php
+++ theme.php
@@ -44,17 +44,17 @@ if (isset($_POST['submit'])) {
         $purge= '0';
 
 	//Add or Edit
-	$query = "INSERT INTO `theme` (`id`, `sync`, `justification`, `purge`, `name`, `background_color`, `text_color`, `border_color`, `footer_color`, `btn_style') VALUES ('{$id}', '{$sync}', '{$purge}', '{$name}', '{$justification}', '{$background_color}', '{$text_color}', '{$border_color}', '{$footer_color}', '{$btn_style}') ON DUPLICATE KEY UPDATE sync=VALUES(sync), `purge`=VALUES(`purge`), name='{$name}', justification='{$justification}', background_color='{$text_color}', name=VALUES(name), type=VALUES(type), on_trigger=VALUES(on_trigger);";
+	$query = "INSERT INTO `theme` (`id`, `sync`, `purge`, `name`, `row_justification`, `background_color`, `text_color`, `border_color`, `footer_color`, `btn_style`) VALUES ('{$id}', '{$sync}', '{$purge}', '{$name}', '{$justification}', '{$background_color}', '{$text_color}', '{$border_color}', '{$footer_color}', '{$btn_style}') ON DUPLICATE KEY UPDATE sync=VALUES(sync), `purge`=VALUES(`purge`), name=VALUES(name), row_justification='{$justification}', background_color='{$background_color}', text_color='{$text_color}', border_color='{$border_color}', footer_color='{$footer_color}', btn_style='{$btn_style}';";
 	$result = $conn->query($query);
         $temp_id = mysqli_insert_id($conn);
 	if ($result) {
                 if ($id==0){
-                        $message_success = "<p>".$lang['relay_record_add_success']."</p>";
+                        $message_success = "<p>".$lang['theme_record_add_success']."</p>";
                 } else {
-                        $message_success = "<p>".$lang['relay_record_update_success']."</p>";
+                        $message_success = "<p>".$lang['theme_record_update_success']."</p>";
                 }
 	} else {
-		$error = "<p>".$lang['relay_record_fail']." </p> <p>" .mysqli_error($conn). "</p>";
+		$error = "<p>".$lang['theme_record_fail']." </p> <p>" .mysqli_error($conn). "</p>";
 	}
 	$message_success .= "<p>".$lang['do_not_refresh']."</p>";
 	header("Refresh: 10; url=home.php");
@@ -87,7 +87,7 @@ if (isset($_POST['submit'])) {
 					<div class="d-flex justify-content-between">
 						<div>
 							<?php if ($id != 0) { echo $lang['edit_theme'] . ": " . $row['name']; }else{
-                		            		echo '<i class="bi bi-plus-square" style="font-size: 1.2rem;"></i>' .' '. $lang['add_theme'];} ?>
+                		            		echo '<i class="bi bi-plus-square" style="font-size: 1.2rem;"></i>&nbsp&nbsp'.$lang['add_theme'];} ?>
 						</div>
 						<div class="btn-group"><?php echo date("H:i"); ?></div>
 					</div>
@@ -95,160 +95,70 @@ if (isset($_POST['submit'])) {
                         	<!-- /.card-header -->
 				<div class="card-body">
 					<form data-bs-toggle="validator" role="form" method="post" action="<?php $_SERVER['PHP_SELF'];?>" id="form-join">
-						<!-- Controller Type -->
-						<div class="form-group" class="control-label"><label><?php echo $lang['controller_type']; ?></label> <small class="text-muted"><?php echo $lang['controller_type_info'];?></small>
-							<select class="form-select" type="text" id="type" name="type" onchange=RelayTypeID(this.options[this.selectedIndex].value)>
-								<?php if(isset($row['type'])) { 
-									switch ($row['type']) {
-										case 0:
-									        	echo '<option selected >Zone</option>';
-									        	break;
-						    				case 1:
-						        				echo '<option selected >Boiler</option>';
-						        				break;
-							                        case 5:
-							                                echo '<option selected >Pump</option>';
-							                                break;
-							    			case 2:
-							        			echo '<option selected >HVAC - Heat</option>';
-							        			break;
-							    			case 3:
-						        				echo '<option selected >HVAC - Chill</option>';
-						       					 break;
-						    				case 4:
-						        				echo '<option selected >HVAC - Fan</option>';
-							        			break;
-									}
-								} ?>
-							        <option value=0>Zone</option>
-							        <option value=1>Boiler</option>
-							        <option value=5>Pump</option>
-							        <option value=2>HVAC - Heat</option>
-						        	<option value=3>HVAC - Chill</option>
-							        <option value=4>HVAC - Fan</option>
-							</select>
-							<div class="help-block with-errors"></div>
-						</div>
-						<input type="hidden" id="type_id" name="type_id" value="<?php if(isset($row['type'])) { echo $row['type']; } else { echo '0'; }?>"/>
-
-						<script language="javascript" type="text/javascript">
-							function RelayTypeID(value)
-								{
-							        var valuetext = value;
-							        var e = document.getElementById("type");
-							        var selected_type_id = e.options[e.selectedIndex].value;
-
-							        document.getElementById("type_id").value = selected_type_id;
-							}
-						</script>
-
-						<!-- Relay Name -->
-						<div class="form-group" class="control-label"><label><?php echo $lang['relay_name']; ?></label> <small class="text-muted"><?php echo $lang['relay_name_info'];?></small>
-							<input class="form-control" placeholder="Relay Name" value="<?php if(isset($row['name'])) { echo $row['name']; } ?>" id="name" name="name" data-bs-error="<?php echo $lang['relay_name_help']; ?>" autocomplete="off" required>
+						<!-- Theme Name -->
+						<div class="form-group" class="control-label"><label class="fs-6"><?php echo $lang['theme_name']; ?></label> <small class="text-muted"><?php echo $lang['theme_name_info'];?></small>
+							<input class="form-control" placeholder="Theme Name" value="<?php if(isset($row['name'])) { echo $row['name']; } ?>" id="name" name="name" data-bs-error="<?php echo $lang['theme_name_help']; ?>" autocomplete="off" required>
 							<div class="help-block with-errors"></div>
 						</div>
 
-						<!-- Relay ID -->
-						<div class="form-group" class="control-label" id="relay_id_label" style="display:block"><label><?php echo $lang['relay_id']; ?></label> <small class="text-muted"><?php echo $lang['relay_id_info'];?></small>
-							<select id="relay_id" onchange=RelayChildList(this.options[this.selectedIndex].value) name="relay_id" class="form-control select2" data-bs-error="<?php echo $lang['relay_id_error']; ?>" autocomplete="off" required>
-								<?php if(isset($rownode['node_id'])) {
-								        echo '<option selected >'.$rownode['node_id']." - ".$rownode['name'].'</option>';
-								        $query = "SELECT id, node_id, name, max_child_id FROM nodes WHERE (name LIKE '%Controller%' OR name LIKE '%Relay%') AND id <> ".$rownode['id']." ORDER BY node_id ASC;";
-								} else {
-							        	$query = "SELECT id, node_id, name, max_child_id FROM nodes WHERE name LIKE '%Controller%' OR name LIKE '%Relay%' ORDER BY node_id ASC;";
-								}
-								$result = $conn->query($query);
-								echo "<option></option>";
-								while ($datarw=mysqli_fetch_array($result)) {
-							        if(strpos($datarw['name'], 'Add-On') !== false) { $max_child_id = 0; } else { $max_child_id = $datarw['max_child_id']; }
-							        echo "<option value=".$datarw['max_child_id'].">".$datarw['node_id']." - ".$datarw['name']."</option>"; } ?>
+						<!-- Justification -->
+						<div class="form-group" class="control-label" id="justify_label" style="display:block"><label class="fs-6"><?php echo $lang['justify']; ?></label> <small class="text-muted"><?php echo $lang['justify_info'];?></small>
+        						<select class="form-select" type="text" id="justification" name="justification" >
+								<?php echo'<option value="left" ' . ($row['row_justification']=="left" ? 'selected' : '') . '>'.$lang['left'].'</option>'; ?>
+                						<?php echo'<option value="center" ' . ($row['row_justification']=="center" ? 'selected' : '') . '>'.$lang['center'].'</option>'; ?>
+                                                                <?php echo'<option value="right" ' . ($row['row_justification']=="right" ? 'selected' : '') . '>'.$lang['right'].'</option>'; ?>
 							</select>
 							<div class="help-block with-errors"></div>
 						</div>
 
-						<script language="javascript" type="text/javascript">
-							function RelayChildList(value)
-								{
-							        var valuetext = value;
-							        var e = document.getElementById("relay_id");
-						        	var selected_relay_id = e.options[e.selectedIndex].text;
-							        var selected_relay_id = selected_relay_id.split(" - ");
-							        var gpio_pins = document.getElementById('gpio_pin_list').value
+                                                <!-- Background Color -->
+                                                <div class="form-group" class="control-label" id="background_color_label" style="display:block"><label class="fs-6"><?php echo $lang['background_color']; ?></label> <small class="text-muted"><?php echo $lang['background_color_info'];?></small>
+                                                        <select class="form-select" type="text" id="background_color" name="background_color" >
+				    				<?php echo'<option value="bg-red" ' . ($row['background_red']=="bg-red" ? 'selected' : '') . '>'.$lang['red'].'</option>'; ?>
+                                                                <?php echo'<option value="bg-orange" ' . ($row['background_color']=="bg-orange" ? 'selected' : '') . '>'.$lang['orange'].'</option>'; ?>
+                               					<?php echo'<option value="bg-primary" ' . ($row['background_color']=="bg-primary" ? 'selected' : '') . '>'.$lang['primary'].'</option>'; ?>
+                                                        </select>
+                                                        <div class="help-block with-errors"></div>
+                                                </div>
 
-							        document.getElementById("selected_relay_id").value = selected_relay_id[0];
+                                                <!-- Text Color -->
+                                                <div class="form-group" class="control-label" id="text_color_label" style="display:block"><label class="fs-6"><?php echo $lang['text_color']; ?></label> <small class="text-muted"><?php echo $lang['text_color_info'];?></small>
+                                                        <select class="form-select" type="text" id="text_color" name="text_color" >
+                                                                <?php echo'<option value="text-white" ' . ($row['text_color']=="text-white" ? 'selected' : '') . '>'.$lang['white'].'</option>'; ?>
+                                                                <?php echo'<option value="text-dark" ' . ($row['background_color']=="text-dark" ? 'selected' : '') . '>'.$lang['black'].'</option>'; ?>
+                                                        </select>
+                                                        <div class="help-block with-errors"></div>
+                                                </div>
 
-							        var opt = document.getElementById("relay_child_id").getElementsByTagName("option");
-							        for(j=opt.length-1;j>=0;j--)
-								        {
-        							        document.getElementById("relay_child_id").options.remove(j);
-        							}
-							        if(selected_relay_id[1].includes("GPIO")) {
-							                var pins_arr = gpio_pins.split(',');
-							                for(j=0;j<=pins_arr.length-1;j++)
-                								{
-							                        var optn = document.createElement("OPTION");
-							                        optn.text = pins_arr[j];
-							                        optn.value = pins_arr[j];
-						        	                document.getElementById("relay_child_id").options.add(optn);
-                							}
-	        						} else {
-								        for(j=0;j<=valuetext;j++)
-        									{
-							                	var optn = document.createElement("OPTION");
-								                optn.text = j;
-							        	        optn.value = j;
-							                	document.getElementById("relay_child_id").options.add(optn);
-        								}
-								}
-							}
-						</script>
-						<input type="hidden" id="selected_relay_id" name="selected_relay_id" value="<?php echo $rownode['node_id']?>"/>
 
-						<!-- Relay Child ID -->
-						<input type="hidden" id="gpio_pin_list" name="gpio_pin_list" value="<?php echo implode(",", array_filter(Get_GPIO_List()))?>"/>
-						<div class="form-group" class="control-label"><label><?php echo $lang['relay_child_id']; ?></label> <small class="text-muted"><?php echo $lang['relay_child_id_info'];?></small>
-						        <select id="relay_child_id" name="relay_child_id" class="form-control select2" data-bs-error="<?php echo $lang['relay_child_id_error']; ?>" autocomplete="off" required>
-						                <?php if(isset($row['relay_child_id'])) {
-						                        echo '<option selected >'.$row['relay_child_id'].'</option>';
-						                        $pos=strpos($rownode["type"], "GPIO");
-						                        if($pos !== false) {
-					        	                        $gpio_list=Get_GPIO_List();
-					                	                for ($x = 0; $x <= count(array_filter($gpio_list)) - 1; $x++) {
-                                        						echo "<option value=".$gpio_list[$x].">".$gpio_list[$x]."</option>";
-                                						}
-	                        					} else {
-						                                for ($x = 1; $x <= $rownode['max_child_id']; $x++) {
-                	                        					echo "<option value=".$x.">".$x."</option>";
-                        	        					}
-					                        	}
-                						} ?>
-				        		</select>
-							<div class="help-block with-errors"></div>
-						</div>
+                                                <!-- Boarder Color -->
+                                                <div class="form-group" class="control-label" id="border_color_label" style="display:block"><label class="fs-6"><?php echo $lang['border_color']; ?></label> <small class="text-muted"><?php echo $lang['border_color_info'];?></small>
+                                                        <select class="form-select" type="text" id="border_color" name="border_color" >
+				                         	<?php echo'<option value="border-red" ' . ($row['border_red']=="border-red" ? 'selected' : '') . '>'.$lang['red'].'</option>'; ?>
+                                                                <?php echo'<option value="border-orange" ' . ($row['border_color']=="border-orange" ? 'selected' : '') . '>'.$lang['orange'].'</option>'; ?>
+                                                                <?php echo'<option value="border-primary" ' . ($row['barder_color']=="border-primary" ? 'selected' : '') . '>'.$lang['primary'].'</option>'; ?>
+                                                        </select>
+                                                        <div class="help-block with-errors"></div>
+                                                </div>
 
-						<!-- Relay On Trigger Level -->
-						<div class="form-group" class="control-label"><label><?php echo $lang['relay_trigger']; ?></label> <small class="text-muted"><?php echo $lang['relay_trigger_info'];?></small>
-							<select class="form-select" type="text" id="on_trigger" name="on_trigger" onchange=OnTrigger(this.options[this.selectedIndex].value)>
-								<?php if(isset($row['on_trigger'])) {
-									if ($row['on_trigger'] == 0) { echo '<option selected >LOW</option>'; } else { echo '<option selected >HIGH</option>'; }
-								} ?>
-								<option value=0>LOW</option>
-					                	<option value=1>HIGH</option>
-						        </select>
-							<div class="help-block with-errors"></div>
-						</div>
-						<input type="hidden" id="trigger" name="trigger" value="<?php if(isset($row['on_trigger'])) { echo $row['on_trigger']; } else { echo '0'; }?>"/>
+                                                <!-- Footer Color -->
+                                                <div class="form-group" class="control-label" id="footer_color_label" style="display:block"><label class="fs-6"><?php echo $lang['footer_color']; ?></label> <small class="text-muted"><?php echo $lang['footer_color_info'];?></small>
+                                                        <select class="form-select" type="text" id="footer_color" name="footer_color" >
+                                    				<?php echo'<option value="card-footer-red" ' . ($row['card-footer-red']=="card-footer-red" ? 'selected' : '') . '>'.$lang['red'].'</option>'; ?>
+                                                                <?php echo'<option value="card-footer-orange" ' . ($row['footer_color']=="card-footer-orange" ? 'selected' : '') . '>'.$lang['orange'].'</option>'; ?>
+                                                                <?php echo'<option value="card-footer-primary" ' . ($row['footer_color']=="card-footer-primary" ? 'selected' : '') . '>'.$lang['primary'].'</option>'; ?>
+                                                        </select>
+                                                        <div class="help-block with-errors"></div>
+                                                </div>
 
-						<script language="javascript" type="text/javascript">
-							function OnTrigger(value)
-								{
-						        	var valuetext = value;
-							        var e = document.getElementById("on_trigger");
-							        var selected_on_trigger = e.options[e.selectedIndex].value;
-
-							        document.getElementById("trigger").value = selected_on_trigger;
-							}
-						</script>
+                                                <!-- Button Style -->
+                                                <div class="form-group" class="control-label" id="btn_style_label" style="display:block"><label class="fs-6"><?php echo $lang['button_style']; ?></label> <small class="text-muted"><?php echo $lang['button_style_info'];?></small>
+                                                        <select class="form-select" type="text" id="btn_style" name="btn_style" >
+                                                                <?php echo'<option value="btn-default-style" ' . ($row['btn_style']=="btn-default-style" ? 'selected' : '') . '>'.$lang['default'].'</option>'; ?>
+                                                                <?php echo'<option value="btn-primary-style" ' . ($row['btn_style']=="btn-primary-style" ? 'selected' : '') . '>'.$lang['primary'].'</option>'; ?>
+                                                        </select>
+                                                        <div class="help-block with-errors"></div>
+                                                </div>
 
 						<!-- Buttons -->
 						<input type="submit" name="submit" value="<?php echo $lang['submit']; ?>" class="btn <?php echo theme($conn, $theme, 'btn_style'); ?> btn-sm">
