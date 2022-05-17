1d0cf518e203761e4bb75b3a167b08913fe11083
twa127 <46624596+twa127@users.noreply.github.com>
2022-05-17 16:47:34 +0100

    Add files via upload

--- header.php
+++ header.php
@@ -65,6 +65,7 @@ $theme = settings($conn, 'theme');
 
 	<!-- Custom Fonts ionicons-->
 	<link rel="stylesheet" type="text/css" media="screen" href="fonts/ionicons-2.0.1/css/ionicons.min.css">
+        <link href="fonts/custom/custom-icons.css" rel="stylesheet" type="text/css">
 
 	<!-- bootstrap-slider
     <link href="css/plugins/slider/bootstrap-slider.min.css" rel="stylesheet">-->
@@ -107,7 +108,7 @@ $theme = settings($conn, 'theme');
             <ul class="nav navbar-top-links ms-auto">
                <li class="dropdown">
                     <a href="index.php">
-                        <i class="bi bi-house-fill" style="font-size: 1.2rem; color: white;"></i>
+                        <i class="bi bi-house-fill <?php echo theme($conn, settings($conn, 'theme'), 'text_color'); ?>" style="font-size: 1.2rem;"></i>
                     </a>
                 </li>
 				<?php // Alert icon need some thinking: May be table with list of alerts and one cron job to check if any thing not communicating.
@@ -120,26 +121,26 @@ $theme = settings($conn, 'theme');
 				?>
                 <li class="dropdown style">
                     <a href="schedule.php">
-                        <i class="bi bi-clock" style="font-size: 1.2rem; color: white;"></i>
+                        <i class="bi bi-clock <?php echo theme($conn, settings($conn, 'theme'), 'text_color'); ?>" style="font-size: 1.2rem;"></i>
                     </a>
                 </li>
                 <li class="dropdown">
                     <a href="chart_open.php">
-                        <i class="bi bi-graph-up" style="font-size: 1.2rem; color: white;"></i>
+                        <i class="bi bi-graph-up <?php echo theme($conn, settings($conn, 'theme'), 'text_color'); ?>" style="font-size: 1.2rem;"></i>
                     </a>
                 </li>
 
                 <li class="dropdown">
                     <a data-bs-toggle="modal" href="#weather" data-bs-backdrop="static" data-bs-keyboard="false">
-                        <i class="bi bi-sun" style="font-size: 1.2rem; color: white;"></i>
+                        <i class="bi bi-sun <?php echo theme($conn, settings($conn, 'theme'), 'text_color'); ?>" style="font-size: 1.2rem;"></i>
                     </a>
                 </li>
 
 		<?php if ($_SESSION['admin'] == 1) { ?>
                 	<!-- /.dropdown-settings -->
                 	<li class="dropdown">
-                    		<a class="dropdown-toggle" data-bs-toggle="dropdown" href="#">
-                        		<i class="bi bi-gear-fill" style="font-size: 1.2rem; color: white;"></i>
+                    		<a class="dropdown-toggle <?php echo theme($conn, settings($conn, 'theme'), 'text_color'); ?>" data-bs-toggle="dropdown" href="#">
+                        		<i class="bi bi-gear-fill <?php echo theme($conn, settings($conn, 'theme'), 'text_color'); ?>" style="font-size: 1.2rem"></i>
                     		</a>
                     		<ul class="dropdown-menu dropdown-user">
                         		<li><a class="dropdown-item" href="settings.php?s_id=1"><i class="bi bi-speedometer" style="font-size: 1rem; color: orange;"></i> <?php echo $lang['system_status']; ?> </a></li>
@@ -161,8 +162,8 @@ $theme = settings($conn, 'theme');
 
 		    <!-- /.dropdown-user -->
                 <li class="dropdown">
-                    <a class="dropdown-toggle" data-bs-toggle="dropdown" href="#">
-                        <i class="bi bi-person-fill" style="font-size: 1.2rem; color: white;"></i>
+                    <a class="dropdown-toggle <?php echo theme($conn, settings($conn, 'theme'), 'text_color'); ?>" data-bs-toggle="dropdown" href="#">
+                        <i class="bi bi-person-fill <?php echo theme($conn, settings($conn, 'theme'), 'text_color'); ?>" style="font-size: 1.2rem;"></i>
                     </a>
                     <ul class="dropdown-menu dropdown-user">
                         <li><a class="dropdown-item" href="user_accounts.php"><i class="bi bi-person-fill" style="font-size: 1.2rem;"></i> <?php echo $lang['user_change_password']; ?> </a></li>
@@ -175,7 +176,7 @@ $theme = settings($conn, 'theme');
                 <?php if ($_SESSION['admin'] == 1 && scan_dir('/var/www/code_updates')) { ?>
                         <li class="dropdown">
                             <a data-bs-toggle="modal" href="#code_update_Modal" data-bs-backdrop="static" data-bs-keyboard="false">
-                                <i class="bi bi-download" style="font-size: 1.2rem; color: white;"></i>
+                                <i class="bi bi-download <?php echo theme($conn, settings($conn, 'theme'), 'text_color'); ?>" style="font-size: 1.2rem;"></i>
                             </a>
                         </li>
                 <?php } ?>
@@ -183,7 +184,7 @@ $theme = settings($conn, 'theme');
                 <?php if ($_SESSION['admin'] == 1 && scan_dir('/var/www/database_updates')) { ?>
                         <li class="dropdown">
                             <a data-bs-toggle="modal" href="#database_update_Modal" data-bs-backdrop="static" data-bs-keyboard="false">
-                                <i class="bi bi-server" style="font-size: 1.2rem; color: white;"></i>
+                                <i class="bi bi-server <?php echo theme($conn, settings($conn, 'theme'), 'text_color'); ?>" style="font-size: 1.2rem;"></i>
                             </a>
                         </li>
                 <?php } ?>
@@ -382,7 +383,7 @@ else
         	<div class="modal-content">
             		<div class="modal-header  <?php echo theme($conn, settings($conn, 'theme'), 'text_color'); ?> <?php echo theme($conn, settings($conn, 'theme'), 'background_color'); ?>">
 				<button type="button" class="close" data-bs-dismiss="modal" aria-hidden="true">x</button>
-                		<h5 class="modal-title"><i class="bi bi-sun" style="font-size: 1.2rem; color: white;"></i> <?php echo $weather['location'] ;?> <?php echo $lang['weather']; ?></h5>
+                		<h5 class="modal-title"><i class="bi bi-sun <?php echo theme($conn, settings($conn, 'theme'), 'text_color'); ?>" style="font-size: 1.2rem;"></i> <?php echo $weather['location'] ;?> <?php echo $lang['weather']; ?></h5>
             		</div>
             		<div class="modal-body">
 				<div class="row">
