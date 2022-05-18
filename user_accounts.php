48e375652b5790c293ea29be86898a3ac87d1871
twa127 <46624596+twa127@users.noreply.github.com>
2022-05-18 21:21:09 +0100

    Add files via upload

--- user_accounts.php
+++ user_accounts.php
@@ -203,23 +203,23 @@ $persist = $row['persist'];
 				<?php if($mode != 0) { ?>
                                 	<div class="row">
                                 		<div class="col-xs-3">
-                                        		<div class="checkbox checkbox-default checkbox-circle">
-								<input id="checkbox0" class="styled" type="checkbox" name="account_enable" value="1" <?php if($aenable == 1 && $mode != 2) { echo 'checked'; } ?> <?php if($id == $_SESSION['user_id']) { echo 'disabled'; } ?> >
-								<label for="checkbox0"> <?php echo $lang['account_enable']; ?> </label> <br><small class="text-muted"><?php echo $lang['account_enable_info'];?></small>
+                                        		<div class="form-check">
+								<input class="form-check-input" type="checkbox" value="1" id="checkbox0" name="account_enable" <?php if($aenable == 1 && $mode != 2) { echo 'checked'; } ?> <?php if($id == $_SESSION['user_id']) { echo 'disabled'; } ?> >
+								<label class="form-check-label" for="checkbox0"> <?php echo $lang['account_enable']; ?> </label> <br><small class="text-muted"><?php echo $lang['account_enable_info'];?></small>
                                                 		<div class="help-block with-errors"></div>
                                         		</div>
 						</div>
 						<div class="col-xs-3">
-                                        		<div class="checkbox checkbox-default checkbox-circle">
-								<input id="checkbox1" class="styled" type="checkbox" name="admin_account" value="1" <?php if($aaccount == 1 && $mode != 2) { echo 'checked'; } ?> <?php if($_SESSION['admin'] == 0) { echo 'disabled'; } ?> >
-								<label for="checkbox1"> <?php echo $lang['admin_account']; ?> </label> <br><small class="text-muted"><?php echo $lang['admin_account_info'];?></small>
+                                        		<div class="form-check">
+								<input class="form-check-input" type="checkbox" value="1" id="checkbox1" name="admin_account" <?php if($aaccount == 1 && $mode != 2) { echo 'checked'; } ?> <?php if($_SESSION['admin'] == 0) { echo 'disabled'; } ?> >
+								<label class="form-check-label" for="checkbox1"> <?php echo $lang['admin_account']; ?> </label> <br><small class="text-muted"><?php echo $lang['admin_account_info'];?></small>
                                                 		<div class="help-block with-errors"></div>
                                         		</div>
 						</div>
 						<div class="col-xs-3">
-                                        		<div class="checkbox checkbox-default checkbox-circle">
-								<input id="checkbox2" class="styled" type="checkbox" name="persist" value="1" <?php if($persist == 1 && $mode != 2) { echo 'checked'; } ?> <?php if($_SESSION['admin'] == 0) { echo 'disabled'; } ?> >
-								<label for="checkbox2"> <?php echo $lang['persist']; ?> </label> <br><small class="text-muted"><?php echo $lang['persist_info'];?></small>
+                                        		<div class="form-check">
+								<input class="form-check-input" type="checkbox" value="1" id="checkbox2" name="persist" <?php if($persist == 1 && $mode != 2) { echo 'checked'; } ?> <?php if($_SESSION['admin'] == 0) { echo 'disabled'; } ?> >
+								<<label class="form-check-label" for="checkbox2"> <?php echo $lang['persist']; ?> </label> <br><small class="text-muted"><?php echo $lang['persist_info'];?></small>
                                                 		<div class="help-block with-errors"></div>
                                         		</div>
 						</div>
