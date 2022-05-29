4bec00e065fb6cf37786a2ed6ddbfa777f9c1046
twa127 <46624596+twa127@users.noreply.github.com>
2022-05-29 08:33:29 +0100

    Create 290522.sql

---
+++ 290522.sql
@@ -0,0 +1,8 @@
+INSERT INTO `theme`(`sync`, `purge`, `name`, `row_justification`, `background_color`, `text_color`, `border_color`, `footer_color`, `btn_style`)
+VALUES (0,0,'Red Left','left','bg-red','text-white','border-red','card-footer-red','btn-default-style');
+INSERT INTO `theme`(`sync`, `purge`, `name`, `row_justification`, `background_color`, `text_color`, `border_color`, `footer_color`, `btn_style`)
+VALUES (0,0,'Red Center','center','bg-red','text-white','border-red','card-footer-red','btn-default-style');
+INSERT INTO `theme`(`sync`, `purge`, `name`, `row_justification`, `background_color`, `text_color`, `border_color`, `footer_color`, `btn_style`)
+VALUES (0,0,'Amber Left','left','bg-amber','text-white','border-amber','card-footer-amber','btn-default-style');
+INSERT INTO `theme`(`sync`, `purge`, `name`, `row_justification`, `background_color`, `text_color`, `border_color`, `footer_color`, `btn_style`)
+VALUES (0,0,'Amber Center','center','bg-amber','text-white','border-amber','card-footer-amber','btn-default-style');
