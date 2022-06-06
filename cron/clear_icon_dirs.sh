13f926703f3d570075d262ca3c31fda946ec0fe6
twa127 <46624596+twa127@users.noreply.github.com>
2022-06-06 08:03:54 +0100

    Create clear_icon_dirs.sh

---
+++ cron/clear_icon_dirs.sh
@@ -0,0 +1,36 @@
+#!/bin/bash
+
+clear
+echo "Clearing Unwanted Icon Directories"
+
+ICON_DIR=/var/www/fonts/ionicons-2.0.1
+echo "Trying to DELETE Directory: $ICON_DIR"
+if [ -d "$ICON_DIR" ];
+ then
+     echo "DELETING Directory: $ICON_DIR"
+     rm -Rf $ICON_DIR
+ else
+  echo "Directory: $ICON_DIR Does Not Exist"
+fi
+
+ICON_DIR=/var/www/fonts/glyphicons
+echo "Trying to DELETE Directory: $ICON_DIR"
+if [ -d "$ICON_DIR" ];
+ then
+     echo "DELETING Directory: $ICON_DIR"
+     rm -Rf $ICON_DIR
+ else
+  echo "Directory: $ICON_DIR Does Not Exist"
+fi
+
+ICON_DIR=/var/www/fonts/ionicons-font-awesome-4.7.0
+echo "Trying to DELETE Directory: $ICON_DIR"
+if [ -d "$ICON_DIR" ];
+ then
+     echo "DELETING Directory: $ICON_DIR"
+     rm -Rf $ICON_DIR
+ else
+  echo "Directory: $ICON_DIR Does Not Exist"
+fi
+
+echo "Finished Clearing Unwanted Icon Directories"
